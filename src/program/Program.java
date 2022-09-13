27d03a9ac8da702a57c79f90bebdfe3d474f9390
Piotr Ludvig Kopczynski <piotrkopczynski52@gmail.com>
2022-09-13 10:15:03 +0200

    Kode fra forelesning 1 i uke 37

--- src/program/Program.java
+++ src/program/Program.java
@@ -19,11 +19,15 @@ public class Program {
          */
         int[] a = Tabell.randPerm(20); // tilfeldig permutasjon av 1 . . 20
         int[] b = Tabell.nestMaks1(a);  // metoden returnerer en tabell
+        int[] c = Tabell.nestMaks4(a);
 
         int m = b[0], nm = b[1];       // m for maks, nm for nestmaks
+        int m2 = c[0], nm2 = c[1];
 
         Tabell.skrivln(a);        // se Oppgave 5 i Avsnitt 1.2.2
         System.out.print("Størst(" + a[m] + ") har posisjon " + m);
         System.out.println(", nest størst(" + a[nm] + ") har posisjon " + nm);
+        System.out.println("Størst: " + m2);
+        System.out.println("Nest størst: " +nm2);
     }
 }
